7361fed3882ff821ac6b1a4cb31c931d592c8c58
Patrick Fung <patrick@makestuffdostuff.com>
2018-09-12 14:35:31 -0400

    generalize styles

--- header.php
+++ header.php
@@ -29,29 +29,6 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'rula_imprinting_canada' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$rula_imprinting_canada_description = get_bloginfo( 'description', 'display' );
-			if ( $rula_imprinting_canada_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $rula_imprinting_canada_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-
-	</header><!-- #masthead -->
-
 	<nav id="site-navigation" class="main-navigation">
 		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rula_imprinting_canada' ); ?></button>
 		<div class="navigation-container">
