9312d9ce37083cecce41dbfaebd7d2f0bd06e2b7
Patrick Fung <patrick@makestuffdostuff.com>
2018-09-20 12:39:06 -0400

    modal stuff

--- functions.php
+++ functions.php
@@ -199,3 +199,62 @@ function rula_imprinting_canada_mce_buttons_2( $buttons ) {
   return $buttons;
 }
 add_filter( 'mce_buttons_2', 'rula_imprinting_canada_mce_buttons_2' );
+
+add_shortcode('ic_media_modal', 'rula_imprinting_canada_media_modal_shortcode');
+function rula_imprinting_canada_media_modal_shortcode($atts = array(), $content = null, $tag) {
+  $attachment_id = $atts['media-id'];
+  $float = $atts['float'];
+  $html = "";
+  $html .= rula_imprinting_canada_media_modal_button($attachment_id, $float);
+  $html .= rula_imprinting_canada_media_modal_content($attachment_id);
+  return $html;
+}
+
+function rula_imprinting_canada_media_modal_button($attachment_id, $float) {
+  $classes = array('btn', 'btn-primary', 'ic_media_modal');
+  if ( $float == 'right' ) {
+    $classes[] = 'float-right';
+  } elseif ( $float == 'left' ) {
+    $classes[] = 'float-left';
+  }
+
+  $html = '<button type="button" class="' . implode(" ", $classes) . '" data-toggle="modal" data-target="#ic_media_' . $attachment_id . '">';
+  $html .= rula_imprinting_canada_media_button_image($attachment_id);
+  $html .= rula_imprinting_canada_media_button_caption($attachment_id);
+  $html .= '</button>';
+
+  return $html;
+}
+
+function rula_imprinting_canada_media_button_image($attachment_id) {
+  return '<img src="' . wp_get_attachment_url($attachment_id) . '">';
+}
+
+function rula_imprinting_canada_media_button_caption($attachment_id) {
+  return '<div class="caption">' . wp_get_attachment_caption($attachment_id) . '</div>';
+}
+
+function rula_imprinting_canada_media_modal_content($attachment_id) {
+  $html = "";
+  $html .= '<div class="modal fade ic_media_modal" id="ic_media_' . $attachment_id . '" tabindex="-1" role="dialog" aria-labelledby="ic_media_' . $attachment_id . '_label" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content">';
+  $html .= rula_imprinting_canada_media_modal_header($attachment_id);
+  $html .= rula_imprinting_canada_media_modal_body($attachment_id);
+  $html .= rula_imprinting_canada_media_modal_footer($attachment_id);
+  $html .= "</div></div></div>";
+
+  return $html;
+}
+
+function rula_imprinting_canada_media_modal_header($attachment_id) {
+  $title = get_the_title($attachment_id);
+  return '<div class="modal-header"><h5 class="modal-title" id="ic_media_' . $attachment_id . '_label">' . $title . '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+}
+
+function rula_imprinting_canada_media_modal_body($attachment_id) {
+  $attachment_url = wp_get_attachment_url($attachment_id);
+  return '<div class="modal-body"><img src="' . $attachment_url . '"></div>';
+}
+
+function rula_imprinting_canada_media_modal_footer($attachment_id) {
+  return '';
+}
