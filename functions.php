e26605de55eff0f917e8695846b80b7a5ee21d13
Patrick Fung <patrick@makestuffdostuff.com>
2018-11-13 11:44:42 -0500

    move template functions file

--- functions.php
+++ functions.php
@@ -174,8 +174,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 require get_template_directory() . '/inc/admin-customizations.php';
 
 // Include our extra theme template functions
-require get_template_directory() . '/inc/ic-template-functions.php';
-
+require get_template_directory() . '/inc/imprinting-canada/ic-template-functions.php';
 
 // Include chapter custom post type
 // include_once( get_template_directory() . '/inc/chapter-cpt.php');
