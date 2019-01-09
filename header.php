27a8369b4a034a51a2a9f3a32e6b0918bb90ce63
Patrick Fung <patrick@makestuffdostuff.com>
2019-01-09 12:48:46 -0500

    update menu button with icons

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'rula_imprinting_canada' ); ?></a>
 
 	<nav id="site-navigation" class="main-navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'rula_imprinting_canada' ); ?></button>
+		<button class="menu-toggle open-menu" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '', 'rula_imprinting_canada' ); ?></button>
 		<div class="navigation-container">
 			<?php
 			wp_nav_menu( array(
